cb264a7f1f2cbef3aa7f391a0614f46940084f69
UnCompa <brandonddxd@gmail.com>
2024-09-18 21:03:47 -0500

    feat: :art: Se agrego cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import "./connect.js";
-
+import cors from 'cors'
 import express from "express";
 import ProductsRoute from "./routes/products/ProductsRoute.js";
 const app = express();
+app.use(cors({
+  origin: "*"
+}))
+app.get("/", (req,res) => {
+  res.send("Bienvenido")
+})
 app.use(express.json())
 app.use("/products",ProductsRoute);
 app.listen(3000);
