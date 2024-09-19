9ebb6839308d6749a11e4bf0a9b6dc84869b65de
Pablo-debug <pablovizuete13@gmail.com>
2024-09-18 19:33:06 -0500

    Rutas Basicas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express from "express";
 import ProductsRoute from "./routes/products/ProductsRoute.js";
 const app = express();
-app.use(ProductsRoute);
+app.use("/products",ProductsRoute);
 app.listen(3000);
--- src/routes/products/ProductsRoute.js
+++ src/routes/products/ProductsRoute.js
@@ -2,8 +2,21 @@ import {Router} from 'express'
 
 const productRoute = Router()
 
-productRoute.get("/", () => {
-  
+productRoute.get("/", (req,res) => {
+  res.send("Hello world")
 })
-
+productRoute.get("/:id",(req,res)=>{
+    const {id}=req.params
+    console.log(id);
+    res.send("Id recuperado"+id)
+})
+productRoute.post("/",(req,res)=>{
+    res.send("Mensage")
+})
+productRoute.put("/:id",(req,res)=>{
+    res.send("Id products")
+})
+productRoute.delete("/:id",(req,res)=>{
+    res.send("Eliminar")
+})
 export default productRoute;
